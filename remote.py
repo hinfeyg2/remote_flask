105c6daaf76b3912d8c8c4655f80e6363e19bbf8
Gavin Hinfey <hinfeyg2@Gavins-Mac-mini.local>
2018-02-23 14:56:45 +0000

    changed os calls to prints

--- remote.py
+++ remote.py
@@ -30,37 +30,37 @@ with open(presets_file, 'r') as infile:
 # single press up
 @app.route("/vol_up_one", methods=["GET"])
 def vol_up_one():
-    os.system("irsend SEND_ONCE AMP vol_up")
+    print("irsend SEND_ONCE AMP vol_up")
     return "volume up one"
 
 # single press down
 @app.route("/vol_down_one", methods=["GET"])
 def vol_down_one():
-    os.system("irsend SEND_ONCE AMP vol_down")
+    print("irsend SEND_ONCE AMP vol_down")
     return "volume down one"
 
 # held press up start
 @app.route("/vol_up_start", methods=["GET"])
 def vol_up_start():
-    os.system("irsend SEND_START AMP vol_up")
+    print("irsend SEND_START AMP vol_up")
     return "start turning up volume"
 
 # held press up stop
 @app.route("/vol_up_stop", methods=["GET"])
 def vol_up_stop():
-    os.system("irsend SEND_STOP AMP vol_up")
+    print("irsend SEND_STOP AMP vol_up")
     return "stop turning up volume"
 
 # held press down start
 @app.route("/vol_down_start", methods=["GET"])
 def vol_down_start():
-    os.system("irsend SEND_START AMP vol_down")
+    print("irsend SEND_START AMP vol_down")
     return "start turning down volume"
 
 # held press down stop
 @app.route("/vol_down_stop", methods=["GET"])
 def vol_down_stop():
-    os.system("irsend SEND_STOP AMP vol_down")
+    print("irsend SEND_STOP AMP vol_down")
     return "stop turning down volume"
 
 
@@ -78,10 +78,10 @@ def select_activity(choice):
                 data[i][y] = selected_activity[i][y]
                 # and run the command
                 if y == "app":
-                    os.system("adb shell input keyevent 3")
-                    os.system(selected_activity[i][y])                        
+                    print("adb shell input keyevent 3")
+                    print(selected_activity[i][y])                        
                 else:
-                    os.system("irsend SEND_ONCE %s %s" % (i, selected_activity[i][y]))
+                    print("irsend SEND_ONCE %s %s" % (i, selected_activity[i][y]))
                     sleep(2)
 
     # write the system state changes back out to json
@@ -94,38 +94,38 @@ def select_activity(choice):
 def select_channel(number):
     for i in str(number):
         input_number = "input_" + i
-        os.system("irsend SEND_ONCE UPC %s" % input_number)
-        os.system("irsend SEND_ONCE UPC down")
+        print("irsend SEND_ONCE UPC %s" % input_number)
+        print("irsend SEND_ONCE UPC down")
         sleep(.3)
-    os.system("irsend SEND_ONCE UPC up")
+    print("irsend SEND_ONCE UPC up")
     return "channel selected " + number
 
 @app.route("/nav/<remote_input>", methods=["GET"])
 def navigate(remote_input):
     if remote_input == "up":
-        os.system("irsend SEND_ONCE UPC up")
+        print("irsend SEND_ONCE UPC up")
         return "upc input up"
 
     elif remote_input == "down":
-        os.system("irsend SEND_ONCE UPC down")
+        print("irsend SEND_ONCE UPC down")
         return "upc input down"
 
     elif remote_input == "left":
-    	os.system("irsend SEND_ONCE UPC up")
-        os.system("irsend SEND_ONCE UPC left")
+        print("irsend SEND_ONCE UPC up")
+        print("irsend SEND_ONCE UPC left")
         return "upc input left"
 
     elif remote_input == "right":
-    	os.system("irsend SEND_ONCE UPC up")
-        os.system("irsend SEND_ONCE UPC right")
+        print("irsend SEND_ONCE UPC up")
+        print("irsend SEND_ONCE UPC right")
         return "upc input right"
 
     elif remote_input == "ok":
-        os.system("irsend SEND_ONCE UPC ok")
+        print("irsend SEND_ONCE UPC ok")
         return "upc input ok"
 
     elif remote_input == "back":
-        os.system("irsend SEND_ONCE UPC back")
+        print("irsend SEND_ONCE UPC back")
         return "upc input back"
         
     else:
